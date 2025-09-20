587970fce70ce26636d7930829b9698d636b3d3e
Gabriel Klein <gabriel.klein@evolutionsoft.com.br>
2025-09-20 10:54:21 -0300

    Feat: select action orders respository and tests

--- src/models/repository/repository_test.py
+++ src/models/repository/repository_test.py
@@ -17,3 +17,91 @@ def test_insert_list_of_documents():
     orders_repository = OrdersRepository(conn)
     my_doc = [{ "elem1": "aqui1"}, {"elem2": "aqui2" }, {"elem3": "aqui3"}]
     orders_repository.insert_list_of_documents(my_doc)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_many():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "valor": 5
+    }
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+
+    for doc in response:
+        print(doc)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_one():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "valor": 5
+    }
+    response = orders_repository.select_one(doc_filter)
+    print()
+    print(response)
+
+    for doc in response:
+        print(doc)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_many_with_properties():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "valor": 5
+    }
+    response = orders_repository.select_many_with_properties(doc_filter)
+    print()
+
+    for doc in response:
+        print(doc)
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_if_property_exists():
+    orders_repository = OrdersRepository(conn)
+    response = orders_repository.select_if_property_exists()
+    print()
+
+    for doc in response:
+        print(doc)
+        print()
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_many_with_multiple_select():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "cupom": True,
+        "itens.doce": { "$exists": True }
+    }
+
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+
+    for doc in response:
+        print(doc)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_select_many_with_or_select():
+    orders_repository = OrdersRepository(conn)
+    doc_filter = {
+        "$or": [
+                { "address": { "exists": False } },
+                { "itens.doce.tipo": "Chocolate" }
+            ]
+    }
+
+    response = orders_repository.select_many(doc_filter)
+    print()
+    print(response)
+
+    for doc in response:
+        print(doc)
+
+def test_selecte_by_object_id():
+    orders_repository = OrdersRepository(conn)
+
+    response = orders_repository.selecte_by_object_id("68ceaad7d9edce8da7d84210")
+    print()
+    print(response)
